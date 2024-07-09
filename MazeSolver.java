31b0a699250e54394b81f651fd8f7e0f072c2ca6
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-07-09 23:01:33 +0900

    keep tracking of destination. line18: y is colum and x is for row.

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,21 +1,21 @@
-import javax.swing.text.Position;
 import java.util.LinkedList;
 
 public class MazeSolver {
 
     static int[][] maze = {
-            {1,0,1,1,0},
-            {1,0,0,0,1},
+            {2,0,1,0,1},
+            {1,1,1,0,1},
             {1,0,1,1,1}
-
     };
     //0 = wall
     //1 = path :stack structure
     //2 = destination
-    LinkedList<Position> path = new LinkedList<>(Position);
+    static LinkedList<Position> path = new LinkedList<Position>();
     public static void main(String[] args) {
-        Position p = new Position();
-        p.x=3;
-        p.y=0;
+        Position p = new Position(4,0);
+        path.push(p);
+
+        maze[path.peek().y][path.peek().x]=0;
+
     }
 }
--- Position.java
+++ Position.java
@@ -2,5 +2,10 @@ public class Position {
     //to keep track of position
     public int x;
     public int y;
+    //create constroctor
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
 
+    }
 }
